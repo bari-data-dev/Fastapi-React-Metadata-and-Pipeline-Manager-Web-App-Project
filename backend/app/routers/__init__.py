f5337a3a7a5a3f37ba77fddec762738d4db72aa9
Bari-asbr <48470795+bari-data-dev@users.noreply.github.com>
2026-08-31 09:25:14 +0700

    Register Produk Distributor router

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,10 +1,12 @@
 from .auth_router import router as auth_router
 from .odists_parsing_router import router as odists_parsing_router
 from .parsing_report_router import router as parsing_report_router
+from .produk_distributor_router import router as produk_distributor_router
 
 
 all_routers = [
     auth_router,
     odists_parsing_router,
     parsing_report_router,
+    produk_distributor_router,
 ]
